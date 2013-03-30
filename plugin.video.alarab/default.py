6336eefbbc2f77da9a079d020aede3e4acb5c330
bisha77 <bachar.khamis@gmail.com>
2013-03-30 23:14:54 +0100

    Uppdated ALARAB plugin
    Implemented needed functions for playing Movies and added new  categorys

--- plugin.video.alarab/default.py
+++ plugin.video.alarab/default.py
@@ -27,12 +27,20 @@ httplib.HTTPResponse.read = patch_http_response_read(httplib.HTTPResponse.read)
 
 
 def CATEGORIES():
-	addDir('مسلسلات عربية ','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9_8',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('مسلسلات كرتون ','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D9%83%D8%B1%D8%AA%D9%88%D9%86_4',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('مسلسلات تركية ','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%AA%D8%B1%D9%83%D9%8A%D8%A9_299',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('مسلسلات اجنبية ','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%A7%D8%AC%D9%86%D8%A8%D9%8A%D8%A9_1951',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('برامج تلفزيون ','http://tv1.alarab.net/view-1_%D8%A8%D8%B1%D8%A7%D9%85%D8%AC-%D8%AA%D9%84%D9%81%D8%B2%D9%8A%D9%88%D9%86_311',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	#addDir('افلام كرتون ','http://tv1.alarab.net/view-1_%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9_1',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('مسلسلات عربية','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9_8',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('افلام عربية','http://tv1.alarab.net/view-1_%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9_1',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('افلام كرتون','http://tv1.alarab.net/view-1_%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D9%83%D8%B1%D8%AA%D9%88%D9%86_295',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('فيديو كليب','http://tv1.alarab.net/view-1_%D9%81%D9%8A%D8%AF%D9%8A%D9%88-%D9%83%D9%84%D9%8A%D8%A8_10',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('برامج تلفزيون','http://tv1.alarab.net/view-1_%D8%A8%D8%B1%D8%A7%D9%85%D8%AC-%D8%AA%D9%84%D9%81%D8%B2%D9%8A%D9%88%D9%86_311',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('مسلسلات كرتون','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D9%83%D8%B1%D8%AA%D9%88%D9%86_4',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('مسلسلات تركية','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%AA%D8%B1%D9%83%D9%8A%D8%A9_299',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('مسلسلات اجنبية','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%A7%D8%AC%D9%86%D8%A8%D9%8A%D8%A9_1951',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('افلام هندية','http://tv1.alarab.net/view-1_افلام-هندية_297',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('افلام اجنبية','http://tv1.alarab.net/view-1_%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%A7%D8%AC%D9%86%D8%A8%D9%8A%D8%A9_5553',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('مقاطع مضحكة ','http://tv1.alarab.net/view-1_%D9%85%D9%82%D8%A7%D8%B7%D8%B9-%D9%85%D8%B6%D8%AD%D9%83%D8%A9_309',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	addDir('مسرحيات','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D8%B1%D8%AD%D9%8A%D8%A7%D8%AA_313',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
+	
+	
 	
 def checkURL(url):
     p = urlparse(url)
@@ -44,102 +52,128 @@ def checkURL(url):
 
 
 def index_series(url):
-    url_mod=url
-    url_mod=url_mod.split("_")
-    
-    for items in url_mod:
-        mooded= str( url_mod[0]).split("-")
-        for elements in mooded:
-            second= mooded[0]
-            first= mooded[1]
-    
-    for i in range(1,20):
-    
-        result_url=second+"-"+str(i)+"_"+url_mod[1]+"_"+url_mod[2]
-        req = urllib2.Request(result_url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        url_ch=(re.compile('<img src="(.+?)" alt="(.+?)"  />').findall(link))
-        url_ch_2=(re.compile('<a href="(.+?)" title="').findall(link))
-        final_series=[]
-        response.close()
-        for items in url_ch_2:
-            if "series" in items:
-                if items not in final_series:
-                    final_series.append(items)
-        for items,elements in itertools.izip(url_ch,final_series):
-            image= items[0]
-            name= items [1]
-            url_serie=elements
-            print name
-            print "http://tv1.alarab.net"+url_serie
-            addDir(name,"http://tv1.alarab.net"+str(url_serie),2,image)
+	try:
+		url_mod=url
+		url_mod=url_mod.split("_")
+		
+		for items in url_mod:
+			mooded= str( url_mod[0]).split("-")
+			for elements in mooded:
+				second= mooded[0]
+				first= mooded[1]
+		
+		for i in range(1,20):
+		
+			result_url=second+"-"+str(i)+"_"+url_mod[1]+"_"+url_mod[2]
+			req = urllib2.Request(result_url)
+			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+			response = urllib2.urlopen(req)
+			link=response.read()
+			url_ch=(re.compile('<img src="(.+?)" alt="(.+?)"  />').findall(link))
+			url_ch_2=(re.compile('<a href="(.+?)" title="').findall(link))
+			final_series=[]
+			response.close()
+			for items in url_ch_2:
+				if "series" in items:
+					if items not in final_series:
+						final_series.append(items)
+			for items,elements in itertools.izip(url_ch,final_series):
+				image= items[0]
+				name= items [1]
+				url_serie=elements
+				print name
+				print "http://tv1.alarab.net"+url_serie
+				addDir(name,"http://tv1.alarab.net"+str(url_serie),2,image)
+	except Exception:
+		print "Exception in index_series "
 
 def list_eposodes(url):
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-    response = urllib2.urlopen(req)
-    link=response.read()
-    url_ch=(re.compile('<a href="(.+?)" title="(.+?)" class="vd_title">').findall(link))
-    response.close()
-    item_list=[]
-    for items in url_ch:
-        for element in items:
-			if items[1] not in item_list:
-				print items[1]
-				addDir(items[1],"http://tv1.alarab.net"+str(items[0]),3,"")
-				item_list.append(items[1])
-				
+    try:
+		req = urllib2.Request(url)
+		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+		response = urllib2.urlopen(req)
+		link=response.read()
+		url_ch=(re.compile('<a href="(.+?)" title="(.+?)" class="vd_title">').findall(link))
+		response.close()
+		item_list=[]
+		for items in url_ch:
+			for element in items:
+				if items[1] not in item_list:
+					print items[1]
+					addDir(items[1],"http://tv1.alarab.net"+str(items[0]),3,"")
+					item_list.append(items[1])
+    except Exception:
+		print "Exception in list_epos "
+					
 def list_films(url):
-    url_mod=url
-    title=""
-    url_mod=url_mod.split("_")
-    
-    for items in url_mod:
-        mooded= str( url_mod[0]).split("-")
-        for elements in mooded:
-            second= mooded[0]
-            first= mooded[1]
-    
-    for i in range(1,20):
-    
-        result_url=second+"-"+str(i)+"_"+url_mod[1]+"_"+url_mod[2]
-        req = urllib2.Request(result_url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
+	try:
+		url_mod=url
+		url_mod=url_mod.split("_")
 		
-        url_ch=(re.compile('<a href="(.+?)" title="مشاهدة فيلم(.+?)">').findall(link))
-        for items in url_ch:
-           
-            if "http://tv1.alarab.net" not in items[0]:
-                if "كامل اون لاين مباشرة يوتيوب كواليتي عالية على العرب بدون تحميل مشاهدة مباشرة مشاهدة فيلم" in items[1]: 
-                    title=str( items[1]).split("كامل اون لاين مباشرة يوتيوب كواليتي عالية على العرب بدون تحميل مشاهدة مباشرة مشاهدة فيلم")
-                elif "كامل مترجم للعربية اون لاين مباشرة كواليتي عالية على العرب بدون تحميل مشاهدة مباشرة مشاهدة فيلم" in items[1]: 
-                    title=str( items[1]).split("كامل مترجم للعربية اون لاين مباشرة كواليتي عالية على العرب بدون تحميل مشاهدة مباشرة مشاهدة فيلم")
-                #print title[0]
-                addDir(title[0],"http://tv1.alarab.net"+str(items[0]),2,"")
-			
+		for items in url_mod:
+			mooded= str( url_mod[0]).split("-")
+			for elements in mooded:
+				second= mooded[0]
+				first= mooded[1]
+		
+		for i in range(1,20):
+		
+			result_url=second+"-"+str(i)+"_"+url_mod[1]+"_"+url_mod[2]
+			req = urllib2.Request(result_url)
+			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+			response = urllib2.urlopen(req)
+			link=response.read()
+			response.close()
+			url_ch=(re.compile('<a href="(.+?)" title="(.+?)dvd اونلاين ">').findall(link))
+			url_ch_2=(re.compile('<img src="(.+?)" alt="(.+?)"  />').findall(link))
+			 
+			for items,elements in itertools.izip( url_ch,url_ch_2):
+				#print items[1]
+				name=str( items[1]).replace("جودة", "")
+				name=name.replace("مشاهدة", "").strip()
+				print name
+				print items[0]
+				print elements[0]
+				addDir(name,"http://tv1.alarab.net"+str(items[0]),3,elements[0])
+	except Exception:
+		print "Exception in list_films "
+	
+				
 
 def get_epos_video(url,name):
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-    response = urllib2.urlopen(req)
-    link=response.read()
-    response.close()
-    url_ch=(re.compile('<a href="(.+?)" target="_blank">').findall(link))
-    url_ch= str(url_ch)
-    url_ch=url_ch.split("url")
-    url_ch=str(url_ch[1].replace("=http://flv.alarab.net/media/videos/flv/",""))
-    url_ch=url_ch.replace("']", "").strip()
-    url_ch="http://flv-origin.alarab.net//flv/"+str(url_ch)
-    print "SERIEURL "+  url_ch
-    addLink(name,url_ch,"") 
+	try:
+		req = urllib2.Request(url)
+		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+		response = urllib2.urlopen(req)
+		link=response.read()
+		response.close()
+		url_ch=(re.compile('<a href="(.+?)" target="_blank">').findall(link))
+		url_ch= str(url_ch)
+		url_ch=url_ch.split("url")
+		url_ch=str(url_ch[1].replace("=http://flv.alarab.net/media/videos/flv/",""))
+		url_ch=url_ch.replace("']", "").strip()
+		if "www.youtube" in url_ch:
+			url_ch=url_ch.split("v=")
+			print "youtube after split: "+str(url_ch)
+			video_id=str(url_ch[1])
+			video_id=video_id.replace(".flv","").strip()
+			print "first item of youtube: "+str(video_id)
+			playback_url = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s' % video_id
+			
+			xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(playback_url)
+			addLink(name,playback_url,"")
+				
+		else:
+
+			url_ch="http://flv-origin.alarab.net//flv/"+str(url_ch)+"?start=0"
+			print "SERIEURL "+  url_ch
+			addLink(name,url_ch,"")
+	except Exception:
+		print "Exception in get_epos_video "
 			
 	
-                
+
+	
 def get_params():
         param=[]
         paramstring=sys.argv[2]
