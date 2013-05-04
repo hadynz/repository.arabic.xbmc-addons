3c48ef21863e693a9a30997912fa55d80769b2d1
bisha77 <bachar.khamis@gmail.com>
2013-05-04 02:43:54 +0200

    made changes

--- plugin.video.arabchannels/default.py
+++ plugin.video.arabchannels/default.py
@@ -32,18 +32,10 @@ httplib.HTTPResponse.read = patch_http_response_read(httplib.HTTPResponse.read)
 
 def CATEGORIES():
 	addDir('Group 1','http://servechannels.itholster.com/HandMeChannels.aspx?country=&typeofapp=tv&epirus=Arabic&typeofchannel=',1,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
-	addDir('Group 2','https://android.glarab.com:457/ajax.aspx?channel=tv&type=free&genre=7',2,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
 	addDir('Group 3',' ',5,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
 	addDir('Group 4','https://www.youtube.com/results?filters=live&search_query=%D8%A8%D8%AB+%D8%AD%D9%8A&page=',6,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
 	addDir('Group 5','https://www.youtube.com/results?search_query=بث+مباشر&filters=live&page=',6,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
 	
-def GLARAB_SUB():
-	addDir('Khaliji Channels','https://android.glarab.com:457/ajax.aspx?channel=tv&type=free&genre=7',3,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
-	addDir('Alsham Channels','https://android.glarab.com:457/ajax.aspx?channel=tv&type=free&genre=8',3,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
-	addDir('North African Channels','https://android.glarab.com:457/ajax.aspx?channel=tv&type=free&genre=9',3,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
-	addDir('Music & Film Channels ','https://android.glarab.com:457/ajax.aspx?channel=tv&type=free&genre=13',3,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
-	addDir('Other TV','https://android.glarab.com:457/ajax.aspx?channel=tv&type=free&genre=12',3,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
-	addDir('News Channels','https://android.glarab.com:457/ajax.aspx?channel=tv&type=free&genre=14',3,'http://www.userlogos.org/files/logos/Rog/livetv_ru_09.png')
 def VideoLinksGroupOne(url):
 	try:
 		req = urllib2.Request(url)
@@ -62,79 +54,6 @@ def VideoLinksGroupOne(url):
 				turl=str(items).replace('"url": "', '') .replace('"', '').strip()
 				response.close()
 				addLink(name,turl,'')
-	except:
-		pass
-def listGglarabMobChannels(url):
-	try:
-		addDir("Aljazeera","https://android.glarab.com:457/ajax.aspx?channel=tv&type=free&chid=303174",4,"http://www.lezha.eu/wp-content/uploads/2012/09/aljazeera-arabic.jpg")
-		req = urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-		response = urllib2.urlopen(req)
-		link=response.read()
-		match_url=(re.compile(';makeHttpRequestNoCache(.+?)">(.+?)</a><img src="(.+?)"/></li>').findall(link))
-		for items in  match_url:
-			path="https://android.glarab.com:457/"+str(items[0]).replace("('", "").replace(");","").replace("&',detailShow,false","").strip()
-			name=str(items[1]).strip()
-			iconimage=str(items[2]).strip()
-			response.close()
-			addDir(name,path,4,iconimage)
-	except:
-		pass
-		
-    
-		
-def playChannelGlarab(name,url):
-	    
-	try:
-	
-		url=str(url).replace("'","").strip()
-		req = urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-		req.add_header('Host','android.glarab.com:457')
-		req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
-		req.add_header('Accept-Language','en-US,en;q=0.5')
-		req.add_header('Accept-Encoding','gzip, deflate')
-			#req.add_header('Cookie',' __utma=64423033.755768911.1366830182.1367054575.1367138660.6; __utmz=64423033.1366830182.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(no')
-		req.add_header('Connection',' keep-alive')
-			
-		response = urllib2.urlopen(req)
-		link=response.read()
-		link=str( link).split("makeHttpRequestNoCache('")
-		response.close()
-		path="https://android.glarab.com:457/"+str( link[1]).replace(');"></span><span class="playVoice" onclick="setRadio(true);', '').replace('false','true').replace("&',initPlayMedia,true","").strip()
-	 
-		if " " in str(path):
-			path=str(path).split("chname=")
-			query_args = { 'chname=':str(path[1])}
-			encoded_args = urllib.urlencode(query_args)
-			url=str(path[0])+ encoded_args
-			
-			req = urllib2.Request(url)
-			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-			response = urllib2.urlopen(req)
-			link=response.read()
-			link=str( link).split(":")
-			target='http://'+str(link[0]).strip()+":7777"+"/"+str(link[1]).strip()+".m3u8"
-			xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(target)
-			addLink(name,target,'')
-		
-		else:
-			
-			
-			req1 = urllib2.Request(path)
-			req1.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-			req1.add_header('Host','android.glarab.com:457')
-			req1.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
-			req1.add_header('Accept-Language','en-US,en;q=0.5')
-			req1.add_header('Accept-Encoding','gzip, deflate')
-			req1.add_header('Connection',' keep-alive')
-			response = urllib2.urlopen(req1)
-			link=response.read()
-			link=str( link).split(":")
-			response.close()
-			target='http://'+str(link[0]).strip()+":7777"+"/"+str(link[1]).strip()+".m3u8"
-			xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(target)
-			addLink(name,target,'')
 	except:
 		pass
 
@@ -250,15 +169,10 @@ if mode==None or url==None or len(url)<1:
 elif mode==1:
         print ""+url
         VideoLinksGroupOne(url)
-elif mode==2:
-        print ""+url
-        GLARAB_SUB()
 elif mode==3:
         print ""+url
         listGglarabMobChannels(url)
-elif mode==4:
-        print ""+url
-        playChannelGlarab(name,url)
+
 elif mode==5:
         print ""+url
         FilmonCh(url)
