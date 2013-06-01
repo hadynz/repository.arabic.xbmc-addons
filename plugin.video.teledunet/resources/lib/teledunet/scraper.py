9e6c1fa7ce94e52b97528ad37d48f83d65cc6568
Robert <robertalmalak7@gmail.com>
2013-06-01 16:22:08 +0200

    Teledunet scraper update (by formee)
    Updated Teledunet scraper code to handle dynamic channels playpath and
    to show channels name after website update. Blue colored HD channels
    get a blue font.

--- plugin.video.teledunet/resources/lib/teledunet/scraper.py
+++ plugin.video.teledunet/resources/lib/teledunet/scraper.py
@@ -43,19 +43,20 @@ def _get_channel_time_player(channel_name):
 
     m = re.search('time_player=(.*);', html, re.M | re.I)
     time_player_str = eval(m.group(1))
+    
 
     m = re.search('curent_media=\'(.*)\';', html, re.M | re.I)
     rtmp_url = m.group(1)
-
-    return rtmp_url, repr(time_player_str).rstrip('0').rstrip('.')
+    play_path= rtmp_url[rtmp_url.rfind("/")+1:] 
+    return rtmp_url, play_path, repr(time_player_str).rstrip('0').rstrip('.')
 
 
 def get_rtmp_params(channel_name):
-    rtmp_url, time_player_id = _get_channel_time_player(channel_name)
+    rtmp_url, play_path, time_player_id = _get_channel_time_player(channel_name)
 
     return {
         'rtmp_url': rtmp_url,
-        'playpath': channel_name,
+        'playpath': play_path,
         'app': 'teledunet',
         'swf_url': ('http://www.teledunet.com/tv/player.swf?'
                     'bufferlength=5&'
@@ -65,8 +66,8 @@ def get_rtmp_params(channel_name):
                     'streamer=%(rtmp_url)s&'
                     'file=%(channel_name)s&'
                     'provider=rtmp'
-                       ) % {'time_player': time_player_id, 'channel_name': channel_name, 'rtmp_url': rtmp_url},
-        'video_page_url': 'http://www.teledunet.com/tv/?channel=%s&no_pub' % channel_name,
+                       ) % {'time_player': time_player_id, 'channel_name': play_path, 'rtmp_url': rtmp_url},
+        'video_page_url': 'http://www.teledunet.com/tv/?channel=%s&no_pub' % play_path,
         'live': '1'
     }
 
@@ -77,8 +78,7 @@ def get_channels():
 
     '''
     If no channels are returned from Teledunet service, fall back to returning
-    channels from an offline HTML fallback file
-    Hady: Unsure in what scenario is this useful given that RTMP stream might also be down as well?!
+    channels from an offline HTML fallback file.
     '''
     if not items:
         path = os.path.join(os.path.dirname(__file__), HTML_FALLBACK)
@@ -93,11 +93,16 @@ def _parse_channels_from_html_dom(html):
 
     for div in html.findAll("div", {"class": "div_channel"}):
         is_working = '#009900' in div['style']
-        label_pattern = '[COLOR green]%s[/COLOR]' if is_working else '%s'
+        is_hd = '#0099ff' in div['style']
+        if is_working:
+           label_pattern = '[COLOR green]%s[/COLOR]'
+        elif is_hd:
+           label_pattern = '[COLOR blue]%s   [/COLOR][COLOR red]HD[/COLOR]'
+        else:
+           label_pattern = '%s'
         path = re.sub('^.*\=', '', div.findAll('a')[1]['href'])
-
         items.append({
-            'title': label_pattern % div.find('font').contents[0],
+            'title': label_pattern % div.find('span').contents[0],
             'thumbnail': div.findAll('img')[0]['src'],
             'path': path
         })
