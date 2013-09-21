55364bbb53c8d72ddb3fc6678d7207dbc5d6452f
bisha77 <bachar.khamis@gmail.com>
2013-09-21 11:14:13 +0200

    Teledunet update to 1.0.9

--- plugin.video.teledunet/default.py
+++ plugin.video.teledunet/default.py
@@ -19,22 +19,6 @@ api = TeledunetAPI(cache)
 @plugin.route('/')
 def list_categories():
     items = [{
-                 'label': 'All',
-                 'path': plugin.url_for('list_all_channels')
-             }, {
-                 'label': 'Browse by Category',
-                 'path': plugin.url_for('browse_by_category')
-             }, {
-                 'label': 'Browse by Network',
-                 'path': plugin.url_for('browse_by_network')
-             }]
-
-    return items
-
-
-@plugin.route('/list/all/')
-def list_all_channels():
-    items = [{
                  'label': channel['title'],
                  'path': plugin.url_for('play_video', url=channel['path']),
                  'is_playable': True,
@@ -44,54 +28,6 @@ def list_all_channels():
     return plugin.finish(items, sort_methods=['label'])
 
 
-@plugin.route('/list/browse_by_category/')
-def browse_by_category():
-    items = [{
-                 'label': category['label'],
-                 'path': plugin.url_for('list_channels_for_category', category_name=category['category_name']),
-             } for category in api.get_channels_grouped_by_category()]
-
-    return plugin.finish(items, sort_methods=['label'])
-
-
-@plugin.route('/list/browse_by_network/')
-def browse_by_network():
-    items = [{
-                 'label': network['label'],
-                 'path': plugin.url_for('list_channels_for_network', network_name=network['network_name'])
-             } for network in api.get_channels_grouped_by_network()]
-
-    return plugin.finish(items, sort_methods=['label'])
-
-
-@plugin.route('/list/category/<category_name>')
-def list_channels_for_category(category_name):
-    channels = api.get_channels()
-    category_channels = api.get_channels_for_category(channels, category_name)
-
-    items = [{
-                 'label': channel['title'],
-                 'path': plugin.url_for('play_video', url=channel['path']),
-                 'is_playable': True,
-                 'thumbnail': channel['thumbnail']
-             } for channel in category_channels]
-
-    return plugin.finish(items, sort_methods=['label'])
-
-
-@plugin.route('/list/network/<network_name>')
-def list_channels_for_network(network_name):
-    channels = api.get_channels()
-    network_channels = api.get_channels_for_network(channels, network_name)
-
-    items = [{
-                 'label': network['title'],
-                 'path': plugin.url_for('play_video', url=network['path']),
-                 'is_playable': True,
-                 'thumbnail': network['thumbnail']
-             } for network in network_channels]
-
-    return plugin.finish(items, sort_methods=['label'])
 
 
 @plugin.route('/play/<url>')
--- plugin.video.teledunet/resources/lib/teledunet/scraper.py
+++ plugin.video.teledunet/resources/lib/teledunet/scraper.py
@@ -40,6 +40,7 @@ def _get_channel_time_player(channel_name):
     req = Request(url)
     req.add_header('Referer', TELEDUNET_URL)    # Simulate request is coming from website
     html = get(req)
+    print html
 
     m = re.search('time_player=(.*);', html, re.M | re.I)
     time_player_str = eval(m.group(1))
@@ -47,7 +48,7 @@ def _get_channel_time_player(channel_name):
 
     m = re.search('curent_media=\'(.*)\';', html, re.M | re.I)
     rtmp_url = m.group(1)
-    play_path= rtmp_url[rtmp_url.rfind("/")+1:] 
+    play_path= rtmp_url[rtmp_url.rfind("/")+1:]
     return rtmp_url, play_path, repr(time_player_str).rstrip('0').rstrip('.')
 
 
@@ -78,7 +79,8 @@ def get_channels():
 
     '''
     If no channels are returned from Teledunet service, fall back to returning
-    channels from an offline HTML fallback file.
+    channels from an offline HTML fallback file
+    Hady: Unsure in what scenario is this useful given that RTMP stream might also be down as well?!
     '''
     if not items:
         path = os.path.join(os.path.dirname(__file__), HTML_FALLBACK)
@@ -92,19 +94,99 @@ def _parse_channels_from_html_dom(html):
     items = []
 
     for div in html.findAll("div", {"class": "div_channel"}):
-        is_working = '#009900' in div['style']
-        is_hd = '#0099ff' in div['style']
-        if is_working:
-           label_pattern = '[COLOR green]%s[/COLOR]'
-        elif is_hd:
-           label_pattern = '[COLOR blue]%s   [/COLOR][COLOR red]HD[/COLOR]'
-        else:
-           label_pattern = '%s'
-        path = re.sub('^.*\=', '', div.findAll('a')[1]['href'])
+        match2=re.compile('''<a href=".+?" onclick="go_c2.?'(.+?)'.+?'rtmp.+?'.?" style_=".+?"><img onerror=".+?" src="(.+?)" height=".+?" style=".+?" /><font style=".+?"> <span id=".+?">(.+?)</span> <font style=".+?">(.+?)</font>''').findall(str(div))
+        match=re.compile('''<a href=".+?" onclick="go_c2.?'(.+?)'.+?'rtmp.+?'.?" style_=".+?"><img onerror=".+?" src="(.+?)" height=".+?" style=".+?" /><font style=".+?"> <span id=".+?">(.+?)</span>''').findall(str(div))
+        for url,thumb,name in match:
+            for url2,thumb2,name2,hd in match2:
+                if name==name2:
+                    name=name+' [COLOR red]HD[/COLOR]'
         items.append({
-            'title': label_pattern % div.find('span').contents[0],
-            'thumbnail': div.findAll('img')[0]['src'],
-            'path': path
+            'title': name,
+            'thumbnail': thumb,
+            'path': url
         })
-
+    items.append({
+            'title': 'MBC',
+            'thumbnail': 'https://si0.twimg.com/profile_images/1133033554/mbc-fb.JPG',
+            'path': 'mbc_1'})
+    items.append({
+            'title': 'JSC +1',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_1'})
+    items.append({
+            'title': 'JSC +2',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_2'})
+    items.append({
+            'title': 'JSC +3',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_3'})
+    items.append({
+            'title': 'JSC +4',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_4'})
+    items.append({
+            'title': 'JSC +5',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_5'})
+    items.append({
+            'title': 'JSC +6',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_6'})
+    items.append({
+            'title': 'JSC +7',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_7'})
+    items.append({
+            'title': 'JSC +8',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_8'})
+    items.append({
+            'title': 'JSC +9',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_9'})
+    items.append({
+            'title': 'JSC +10',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'jsc_10'})
+    items.append({
+            'title': 'Abu Dhabi Al Oula',
+            'thumbnail': 'https://www.zawya.com/pr/images/2009/ADTV_One_RGB_2009_10_08.jpg',
+            'path': 'abu_dhabi'})
+    items.append({
+            'title': 'Abu Dhabi Sports',
+            'thumbnail': 'https://si0.twimg.com/profile_images/2485587448/2121.png',
+            'path': 'abu_dhabi_sports_1'})
+    items.append({
+            'title': 'Al Jazeera',
+            'thumbnail': 'http://www.chicagonow.com/chicago-sports-media-watch/files/2013/04/Al-Jazeera.jpg',
+            'path': 'aljazeera'})
+    items.append({
+            'title': 'Al Jazeera Sport 1',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'aljazeera_sport_1'})
+    items.append({
+            'title': 'Al Jazeera Sport 2',
+            'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
+            'path': 'aljazeera_sport_2'})
+    items.append({
+            'title': 'Al Jazeera Mubasher Masr',
+            'thumbnail': 'http://www.chicagonow.com/chicago-sports-media-watch/files/2013/04/Al-Jazeera.jpg',
+            'path': 'aljazeera_mubasher_masr'})
+    items.append({
+            'title': 'ART Aflam 2',
+            'thumbnail': 'http://www.invision.com.sa/en/sites/default/files/imagecache/216x216/channels/2011/10/11/1138.jpg',
+            'path': 'art_aflam_2'})
+    items.append({
+            'title': 'Al Jazeera Children',
+            'thumbnail': 'http://www.chicagonow.com/chicago-sports-media-watch/files/2013/04/Al-Jazeera.jpg',
+            'path': 'aljazeera_children'})
+    items.append({
+            'title': 'ART Cinema',
+            'thumbnail': 'http://www.lyngsat-logo.com/hires/aa/art_cinema.png',
+            'path': 'art_aflam_1'})
+    items.append({
+            'title': 'Cartoon Network',
+            'thumbnail': 'http://upload.wikimedia.org/wikipedia/commons/b/bb/Cartoon_Network_Arabic_logo.png',
+            'path': 'cartoon_network'})
     return items
