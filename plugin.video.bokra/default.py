c650516bb969706fe88fc9b5409c3186c8c44c66
bisha77 <bachar.khamis@gmail.com>
2013-11-18 18:47:43 +0100

    Fixed episodes indexing in Bokra

--- plugin.video.bokra/default.py
+++ plugin.video.bokra/default.py
@@ -31,8 +31,8 @@ httplib.HTTPResponse.read = patch_http_response_read(httplib.HTTPResponse.read)
 
 
 def CATEGORIES():
-	addDir('مسلسلات رمضان 2013','http://www.bokra.net/Sections/1267/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%B1%D9%85%D8%B6%D8%A7%D9%86-2013',1,'http://images.bokra.net/bokra//28-11-2010/4shobek.jpg')
-	addDir('مسلسلات عربية','http://www.bokra.net/VideoCategory/98/مسلسلات_عربية.html',1,'http://images.bokra.net/bokra//28-11-2010/4shobek.jpg')
+	addDir('مسلسلات رمضان 2013','http://www.bokra.net/VideoCategory/125/مسلسلات_رمضان_2013.html',6,'http://images.bokra.net/bokra//28-11-2010/4shobek.jpg')
+	addDir('مسلسلات عربية','http://www.bokra.net/VideoCategory/98/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA_%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9.html',1,'http://images.bokra.net/bokra//28-11-2010/4shobek.jpg')
 	addDir('مسلسلات متنوعة','http://www.bokra.net/VideoCategory/43/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA.html',1,'http://images.bokra.net/bokra//28-11-2010/4shobek.jpg')
 	addDir('افلام عربية','http://www.bokra.net/VideoCategory/100/أفلام_عربية.html',4,'http://images.bokra.net/bokra//25-11-2012/0777777.jpg')
 	addDir(' افلام فلسطينية','http://www.bokra.net/VideoCategory/18/%D8%A7%D9%81%D9%84%D8%A7%D9%85_%D9%81%D9%84%D8%B3%D8%B7%D9%8A%D9%86%D9%8A%D8%A9.html',4,'http://images.bokra.net/bokra//25-11-2012/0777777.jpg')
@@ -104,7 +104,7 @@ def index(url):
 			link=response.read()
 			response.close()
 			
-			url_ch=(re.compile('<div class="pic"><a href="(.+?)" onClick="(.+?);"><img src="(.+?)" width="71" height="57" border="0" alt="(.+?)" title="').findall(link))
+			url_ch=(re.compile('<div class="pic"><a href="(.+?)" onClick="(.+?);"><img class="lazy" data-original="(.+?)" width="(.+?)" title="').findall(link))
 			if len(str(url_ch))<3:
 				url_ch=(re.compile('<div class="pic"><a href="(.+?)" onClick="javascript:(.+?);"><img class="lazy" data-original="(.+?)" width="139" height="96"').findall(link))
 			
@@ -139,9 +139,83 @@ def index(url):
 					
 					image=final_items.pop(0)
 					addDir(name,url,2,image)
+					
 	except Exception:
 		print "Film series Exception occured"
 
+def indexRamadanSeries(url):
+    
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    
+    matchSerie = re.compile(' <div class="items">(.+?)<div class="bigBanner">', re.DOTALL).findall(link)
+    for items in matchSerie:
+        
+        myTarget=str( items).split('<div class="item">')
+        for itr in myTarget:
+            mySecTarget=str( itr).split('/></a></div>')
+            mytempPath= mySecTarget[0] 
+            if 'spacer8' not in str(mytempPath):
+                mypath=str(mytempPath).replace('<div class="pic"><a href="', 'del').replace('onClick="javascript: pageTracker._trackPageview(', 'del').replace(');"><img class="lazy" data-original="',"del").replace('title="','del')
+                mypath=str(mypath).split('del')
+                finalImage=str( mypath[3]).split('" width=')[0]
+                finalName=str( mypath[4]).replace('"', '').strip()
+                finalImage=str(finalImage).strip()
+                finalUrl=str( mypath[1]).replace('"', '').strip()
+                addDir(finalName,finalUrl,2,finalImage)
+
+def indexMySeries(url):
+    
+	try:
+	
+		req = urllib2.Request(url)
+		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+		response = urllib2.urlopen(req)
+		link=response.read()
+		response.close()
+		matchSerie = re.compile(' <div class="items">(.+?)<div class="bigBanner">', re.DOTALL).findall(link)
+		for items in matchSerie:
+			myPath=str( items).split('<div class="item">')
+			for reg in  myPath:
+				myP= str(myPath[1]).split('<div class="spacer3"></div>')
+				myFinalPath= str(myP[0]).split('/></a></div')[0]
+				mypath=str(myFinalPath).replace('<div class="pic"><a href="', 'del').replace('onClick="javascript: pageTracker._trackPageview(', 'del').replace(');"><img class="lazy" data-original="',"del").replace('title="','del')
+				mypath=str(mypath).split('del')
+				finalImage=str( mypath[3]).split('" width=')[0]
+				finalName=str( mypath[4]).replace('"', '').strip()
+				finalImage=str(finalImage).strip()
+				finalUrl=str( mypath[1]).replace('"', '').strip()
+				addLink(finalName,finalUrl,3,finalImage)
+	except:
+		pass
+			
+			
+			
+				
+def indexNewSeries(url):
+    
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    
+    matchSerie = re.compile(' <div class="video_box">(.+?)<div class="spacer_videobox"></div>', re.DOTALL).findall(link)
+    for item in matchSerie:
+        myTempTarget=str(item).split('</div>')
+        tempAll= str(myTempTarget[2]).replace('<div class ="textarea">', '').strip()
+       
+        mypath=str(tempAll).replace('onClick="javascript: pageTracker._trackPageview(', 'del').replace(';" title="', 'del').replace('">  <div class="title">',"del")
+        mypath=str(mypath).split('del')
+        myName=str( mypath[2]).strip()
+       
+        myUrl=str( mypath[0]).replace('<a href="', '').replace('"', '').strip()
+        print myName 
+        addLink(myName,myUrl,3,'')
+		
 def index_films(url):
 	try:
 		counter=0
@@ -204,43 +278,52 @@ def listSeries(url):
 	final_items=[]
 	kurl=url
 	maxvalue=int(retrive_max_page(kurl))+5
+	pointer=False
 	
 	print "THIS IS PAGENR" +str(maxvalue)
 	
         for counter in range(1,int(maxvalue)):
-            test_url=kurl+'/'+str(counter)
+			test_url=kurl+'/'+str(counter)
             
-            req = urllib2.Request(test_url)
-            req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-            response = urllib2.urlopen(req)
-            link=response.read()
+			req = urllib2.Request(test_url)
+			req.add_header('Host', 'www.bokra.net')
+			req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+			req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36')
+			req.add_header('Accept-Encoding', 'gzip,deflate,sdch')
+			req.add_header('Accept-Language', 'sv-SE,sv;q=0.8,en-US;q=0.6,en;q=0.4')
+			req.add_header('Cookie', '__gads=ID=caaace40ba968885:T=1384540808:S=ALNI_MaJHS-I3T2KjuJwJrFhK-WSSHuOLg; PHPSESSID=o5557ubn6glmber62cpcs0vde4; __utma=1.2064279049.1384540807.1384689773.1384693281.3; __utmb=1.8.10.1384693281; __utmc=1; __utmz=1.1384540807.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); noadvtday=0; __atuvc=5%7C47')
+			response = urllib2.urlopen(req)
+			link=response.read()
            
-            response.close()
+			response.close()
             
-            url_ch=(re.compile('<div class="pic"><a href="(.+?)" onClick="javascript:(.+?);"><img src="(.+?)" width="139" height="96').findall(link))
-            if len(str(url_ch))<3:
+			url_ch=(re.compile('<div class="pic"><a href="(.+?)" onClick="javascript:(.+?);"><img class="lazy" data-original="(.+?)" width="').findall(link))
+			if len(str(url_ch))<3:
 				url_ch=(re.compile('<div class="pic"><a href="(.+?)" onClick="javascript:(.+?);"><img src="(.+?)" width="147" height="107').findall(link))
 			
      
-            for items in url_ch:
-                for elements in items:
+			for items in url_ch:
+				for elements in items:
                     
-                    for i in items:
-                        url= items[0].strip()
-                        name= items[1].replace("pageTracker._trackPageview('/VideoAlbum/","")
-                        name=name.replace("html","")
-                        name=name.replace(".')","")
-                        name=name.rsplit("/",1)
-                        name = name[1].strip()  
-                        image= items[2].strip()
-                        if url not in final_items:
-                            if 'اعلان' not in name:
-                                final_items.append(name)
-                                final_items.append(url)
-                                final_items.append(image)
-        
+					for i in items:
+						url= items[0].strip()
+						name= items[1].replace("pageTracker._trackPageview('/VideoAlbum/","")
+						name=name.replace("html","")
+						name=name.replace(".')","")
+						name=name.rsplit("/",1)
+						name = name[1].strip()  
+						image= items[2].strip()
+						if url not in final_items:
+							if 'اعلان' not in name:
+								
+								final_items.append(name)
+								final_items.append(url)
+								final_items.append(image)
+								pointer=True
+			
 			
 				addLink(name,url,3,image)
+			return pointer
 				
 
 def VideoLinks(name,url):
@@ -428,14 +511,6 @@ def get_params():
 
 
 
-
-def addLinkOLD(name,url,iconimage):
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
-        return ok
-
 def addLink(name,url,mode,iconimage):
     u=_pluginName+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
     ok=True
@@ -489,8 +564,10 @@ elif mode==1:
         index(url)
 	
 elif mode==2:
-        print ""+url
-        listSeries(url)
+		print ""+url
+		if listSeries(url)==False:
+			indexNewSeries(url)
+			
 elif mode==3:
 	print ""+url
 	VideoLinks(name,url)
@@ -501,6 +578,9 @@ elif mode==4:
 elif mode==5:
 	print ""+url
 	VideoLinks_Films(name,url)
+elif mode==6:
+	print ""+url
+	indexRamadanSeries(url)
 	
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
