d180245db65d0022af0fa774d278900f62527f65
relax1000 <malkani.romario@googlemail.com>
2014-04-18 10:01:08 +0200

    Teledunet Fix
    Fixed Script Error

--- plugin.video.teledunet/resources/lib/teledunet/hardcode.py
+++ plugin.video.teledunet/resources/lib/teledunet/hardcode.py
@@ -3,45 +3,45 @@ HARDCODED_STREAMS = [{
                          'thumbnail': 'https://si0.twimg.com/profile_images/1133033554/mbc-fb.JPG',
                          'path': 'mbc_1'},
                      {
-                         'title': 'JSC +1',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_1'},
+                         'title': 'BeIn Sport 1',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_1'},
                      {
-                         'title': 'JSC +2',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_2'},
+                         'title': 'BeIn Sport 2',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_2'},
                      {
-                         'title': 'JSC +3',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_3'},
+                         'title': 'BeIn Sport 3',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_3'},
                      {
-                         'title': 'JSC +4',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_4'},
+                         'title': 'BeIn Sport 4',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_4'},
                      {
-                         'title': 'JSC +5',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_5'},
+                         'title': 'BeIn Sport 5',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_5'},
                      {
-                         'title': 'JSC +6',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_6'},
+                         'title': 'BeIn Sport 6',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_6'},
                      {
-                         'title': 'JSC +7',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_7'},
+                         'title': 'BeIn Sport 7',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_7'},
                      {
-                         'title': 'JSC +8',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_8'},
+                         'title': 'BeIn Sport 8',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_8'},
                      {
-                         'title': 'JSC +9',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_9'},
+                         'title': 'BeIn Sport 9',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_9'},
                      {
-                         'title': 'JSC +10',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'jsc_10'},
+                         'title': 'BeIn Sport 10',
+                         'thumbnail': 'http://www.teledunet.com//tv_/icones/bein_sport_1.jpg',
+                         'path': 'bein_sport_10'},
                      {
                          'title': 'Abu Dhabi Al Oula',
                          'thumbnail': 'https://www.zawya.com/pr/images/2009/ADTV_One_RGB_2009_10_08.jpg',
@@ -50,22 +50,35 @@ HARDCODED_STREAMS = [{
                          'title': 'Abu Dhabi Sports',
                          'thumbnail': 'https://si0.twimg.com/profile_images/2485587448/2121.png',
                          'path': 'abu_dhabi_sports_1'},
+
                      {
                          'title': 'Al Jazeera',
                          'thumbnail': 'http://www.chicagonow.com/chicago-sports-media-watch/files/2013/04/Al-Jazeera.jpg',
                          'path': 'aljazeera'},
                      {
-                         'title': 'Al Jazeera Sport 1',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'aljazeera_sport_1'},
+                         'title': 'Aljadeed',
+                         'thumbnail': 'http://libnanews.com/wp-content/uploads/2011/01/al-jadeed-logo.jpg',
+                         'path': 'aljadeed'},
                      {
-                         'title': 'Al Jazeera Sport 2',
-                         'thumbnail': 'http://nowwatchtvlive.com/wp-content/uploads/2011/07/AljazeeraSport-264x300.jpg',
-                         'path': 'aljazeera_sport_2'},
+                         'title': 'OTV Lebanon',
+                         'thumbnail': 'https://si0.twimg.com/profile_images/3386875020/31bfc3b7880c810620288392148323ce.jpeg',
+                         'path': 'otv_lebanon'},
                      {
-                         'title': 'Al Jazeera Mubasher Masr',
-                         'thumbnail': 'http://www.chicagonow.com/chicago-sports-media-watch/files/2013/04/Al-Jazeera.jpg',
-                         'path': 'aljazeera_mubasher_masr'},
+                         'title': 'Al Nahar',
+                         'thumbnail': 'http://2.bp.blogspot.com/-0Efo2UMoGbA/Tikieupg-9I/AAAAAAAAB3E/ILjst3MG4UU/s200/al-nahar%2Bchaneel.JPG',
+                         'path': 'al_nahar'},
+                     {
+                         'title': 'Al Hayat 1',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'alhayat_1'},
+                     {
+                         'title': 'Al Hayat Series',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'alhayat_series'},
+                     {
+                         'title': 'Al Hayat Cinema',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'alhayat_cinema'},
                      {
                          'title': 'ART Aflam 2',
                          'thumbnail': 'http://www.invision.com.sa/en/sites/default/files/imagecache/216x216/channels/2011/10/11/1138.jpg',
@@ -78,6 +91,82 @@ HARDCODED_STREAMS = [{
                          'title': 'ART Cinema',
                          'thumbnail': 'http://www.lyngsat-logo.com/hires/aa/art_cinema.png',
                          'path': 'art_aflam_1'},
+                     {
+                         'title': 'Tele Liban',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'tele_liban'},
+                     {
+                         'title': 'Al Hayat 2',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'alhayat_2'},
+                     {
+                         'title': 'Alarabiya',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'alarabiya'},
+                     {
+                         'title': 'Al Moutawassit',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'al_mutawasit'},
+                     {
+                         'title': 'CBC',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'cbc'},
+                     {
+                         'title': 'CBC Extra',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'cbc_extra'},
+                     {
+                         'title': 'CBC 2',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'cbc_extra'},
+                     {
+                         'title': 'CBC Sofra',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'cbc_sofra'},
+                     {
+                         'title': 'Al Rafidain',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'al_rafidain'},
+                     {
+                         'title': 'Dzair 24',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'dzair_24'},
+                     {
+                         'title': 'NBN',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'nbn'},
+                     {
+                         'title': 'Panorama Drama 2',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'panorama_drama_2'},
+                     {
+                         'title': 'Panorama Drama',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'panorama_drama'},
+                     {
+                         'title': 'Panorama Comedy',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'panorama_comedy'},
+                     {
+                         'title': 'Sada El Balad +2',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'sada_albalad'},
+                     {
+                         'title': 'Libya AlAhrar',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'libya_alahrar'},
+                     {
+                         'title': 'Libya Alwatania 1',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'libya_alwatania_1'},
+                     {
+                         'title': 'Dzair TV',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'dzair_tv'},
+                     {
+                         'title': 'Sada El Balad',
+                         'thumbnail': 'http://3.bp.blogspot.com/--uP1DsoBB7s/T4EMosYH5uI/AAAAAAAAF9E/RdbY8-E3Riw/s320/Al%2Bhayat.jpg',
+                         'path': 'sada_el_balad'},
                      {
                          'title': 'Cartoon Network',
                          'thumbnail': 'http://upload.wikimedia.org/wikipedia/commons/b/bb/Cartoon_Network_Arabic_logo.png',
--- plugin.video.teledunet/resources/lib/teledunet/scraper.py
+++ plugin.video.teledunet/resources/lib/teledunet/scraper.py
@@ -5,10 +5,12 @@ from BeautifulSoup import BeautifulSoup
 from models import ChannelItem
 from hardcode import HARDCODED_STREAMS
 
-HEADER_REFERER = 'http://www.teledunet.com/'
+#HEADER_REFERER = 'http://www.teledunet.com/'
+HEADER_REFERER = 'http://www.teledunet.com/list_chaines.php'
 HEADER_HOST = 'www.teledunet.com'
 HEADER_USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-TELEDUNET_TIMEPLAYER_URL = 'http://www.teledunet.com/tv_/?channel=%s'
+TELEDUNET_TIMEPLAYER_URL = 'http://www.teledunet.com/player/?channel=%s'
+PPV_CHANNEL_URL='rtmp://5.135.134.110:1935/teledunet/'
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
@@ -21,7 +23,7 @@ def _get(request):
 def _html(url):
     """Downloads the resource at the given url and parses via BeautifulSoup"""
     headers = { "User-Agent": HEADER_USER_AGENT  }
-    request = urllib2.Request (url , headers = headers) 	
+    request = urllib2.Request (url , headers = headers)
     return BeautifulSoup(_get(request), convertEntities=BeautifulSoup.HTML_ENTITIES)
 
 
@@ -38,7 +40,6 @@ def __get_cookie_session():
 
 def __get_channel_time_player(channel_name):
     url = TELEDUNET_TIMEPLAYER_URL % channel_name
-
     # Set custom header parameters to simulate request is coming from website
     req = urllib2.Request(url)
     req.add_header('Referer', HEADER_REFERER)
@@ -47,12 +48,14 @@ def __get_channel_time_player(channel_name):
     req.add_header('Cookie', __get_cookie_session())
 
     html = _get(req)
-
     m = re.search('time_player=(.*);', html, re.M | re.I)
     time_player_str = eval(m.group(1))
 
     m = re.search('curent_media=\'(.*)\';', html, re.M | re.I)
-    rtmp_url = m.group(1)
+    if 'bein_sport' in channel_name:
+        rtmp_url=PPV_CHANNEL_URL+channel_name
+    else:
+        rtmp_url = m.group(1)
     play_path = rtmp_url[rtmp_url.rfind("/") + 1:]
     return rtmp_url, play_path, repr(time_player_str).rstrip('0').rstrip('.')
 
@@ -64,23 +67,16 @@ def get_rtmp_params(channel_name):
         'rtmp_url': rtmp_url,
         'playpath': play_path,
         'app': 'teledunet',
-        'swf_url': ('http://www.teledunet.com/tv/player.swf?'
-                    'bufferlength=5&'
-                    'repeat=single&'
-                    'autostart=true&'
+        'swf_url': ('http://www.teledunet.com/player.swf?'
                     'id0=%(time_player)s&'
-                    'streamer=%(rtmp_url)s&'
-                    'file=%(channel_name)s&'
-                    'provider=rtmp'
                    ) % {'time_player': time_player_id, 'channel_name': play_path, 'rtmp_url': rtmp_url},
-        'video_page_url': 'http://www.teledunet.com/tv/?channel=%s&no_pub' % play_path,
+        'video_page_url': 'http://www.teledunet.com/player/?channel=%s' % play_path,
         'live': '1'
     }
 
-
 def get_channels():
     html = _html(HEADER_REFERER)
-    channel_divs = lambda soup : soup.findAll("div", { "class" : re.compile("div_channel") }) 
+    channel_divs = lambda soup : soup.findAll("div", { "class" : re.compile("div_channel") })
     channels = [ChannelItem(el=el) for el in channel_divs(html)]
 
     # Extend Teledunet list with custom hardcoded list created by community
