ed51af8bc5247264a75145817d8577ae478e8e26
relax1000 <malkani.romario@googlemail.com>
2014-10-03 10:48:10 +0200

    AL Arab Fix
    Thanks to daschacka

--- plugin.video.alarab/default.py
+++ plugin.video.alarab/default.py
@@ -1,242 +1,117 @@
 # -*- coding: utf8 -*-
-import urllib,urllib2,re,xbmcplugin,xbmcgui
-import xbmc, xbmcgui, xbmcplugin, xbmcaddon
-from httplib import HTTP
-from urlparse import urlparse
-import StringIO
-import urllib2,urllib
-import re
-import httplib,itertools
+import urllib,urllib2,xbmcplugin,xbmcgui,xbmcaddon
 
-import time
-
-
-__settings__ = xbmcaddon.Addon(id='plugin.video.alarab')
+__settings__ = xbmcaddon.Addon(id='plugin.video.alarabnet')
 __icon__ = __settings__.getAddonInfo('icon')
 __fanart__ = __settings__.getAddonInfo('fanart')
 __language__ = __settings__.getLocalizedString
 _thisPlugin = int(sys.argv[1])
 _pluginName = (sys.argv[0])
 
-def patch_http_response_read(func):
-    def inner(*args):
-        try:
-            return func(*args)
-        except httplib.IncompleteRead, e:
-            return e.partial
 
-    return inner
-httplib.HTTPResponse.read = patch_http_response_read(httplib.HTTPResponse.read)
 
 
 def CATEGORIES():
-	#xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%('WARNING','This addon is completely FREE DO NOT buy any products from http://tvtoyz.com/', 16000, 'http://wadeni.com/images/icons/0alarab-net.jpg'))
-	addDir('مسلسلات عربية','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9_8',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('افلام عربية','http://tv1.alarab.net/view-1_%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9_1',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('افلام كرتون','http://tv1.alarab.net/view-1_%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D9%83%D8%B1%D8%AA%D9%88%D9%86_295',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('فيديو كليب','http://tv1.alarab.net/view-1_%D9%81%D9%8A%D8%AF%D9%8A%D9%88-%D9%83%D9%84%D9%8A%D8%A8_10',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('برامج تلفزيون','http://tv1.alarab.net/view-1_%D8%A8%D8%B1%D8%A7%D9%85%D8%AC-%D8%AA%D9%84%D9%81%D8%B2%D9%8A%D9%88%D9%86_311',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('مسلسلات كرتون','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D9%83%D8%B1%D8%AA%D9%88%D9%86_4',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('مسلسلات تركية','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%AA%D8%B1%D9%83%D9%8A%D8%A9_299',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('مسلسلات اجنبية','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%A7%D8%AC%D9%86%D8%A8%D9%8A%D8%A9_1951',1,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('افلام هندية','http://tv1.alarab.net/view-1_افلام-هندية_297',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('افلام اجنبية','http://tv1.alarab.net/view-1_%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%A7%D8%AC%D9%86%D8%A8%D9%8A%D8%A9_5553',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('مقاطع مضحكة ','http://tv1.alarab.net/view-1_%D9%85%D9%82%D8%A7%D8%B7%D8%B9-%D9%85%D8%B6%D8%AD%D9%83%D8%A9_309',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	addDir('مسرحيات','http://tv1.alarab.net/view-1_%D9%85%D8%B3%D8%B1%D8%AD%D9%8A%D8%A7%D8%AA_313',4,'http://www.alfnnews.com/files/pic/2012/5/19/2012561916144-alarb.gif')
-	
-	
-	
-	
-def checkURL(url):
-    p = urlparse(url)
-    h = HTTP(p[1])
-    h.putrequest('HEAD', p[2])
-    h.endheaders()
-    if h.getreply()[0] == 200: return 1
-    else: return 0
+	addDir("Aflam","http://tv1.alarab.net/view-1_%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9_1",1,"http://wadeni.com/images/icons/0alarab-net.jpg")
+	addDir("Serie","http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9_8",2,"http://wadeni.com/images/icons/0alarab-net.jpg")
+	addDir("Serie Ajnabi","http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%A7%D8%AC%D9%86%D8%A8%D9%8A%D8%A9_1951",2,"http://wadeni.com/images/icons/0alarab-net.jpg")
+	addDir("Serie Turki","http://tv1.alarab.net/view-1_%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%AA%D8%B1%D9%83%D9%8A%D8%A9_299",2,"http://wadeni.com/images/icons/0alarab-net.jpg")
 
+def getMovie(url):
+	openerx = urllib2.build_opener()
+	sockx = openerx.open(url)
+	contentx = sockx.read() 
+	sockx.close()
+	wieviele = contentx.count('<div class="video-box">')
+	teilen = contentx.split('<div class="video-box">')
+	for i in range(1,wieviele+1):
+		linkjetzt = teilen[i].split('"')
+		imgjetzt = linkjetzt[3]
+		urljetzt = "http://tv1.alarab.net/"+linkjetzt[1]
+		namejetzt = linkjetzt[5]
+		addLink(namejetzt,urljetzt,4,imgjetzt)
+	seitenzahl1 = contentx.split('<div class="pages"><center>')
+	seitenzahl2 = seitenzahl1[1].split("</div></center></div>")
+	seitenzahl3 = seitenzahl2[0].split('tsc_3d_button blue"')
+	seitenzahl4 = seitenzahl3[1].split(">")
+	seitenzahl5 = seitenzahl4[1].split("<")
+	seitenzahlselected = seitenzahl5[0]
+	seitenwieviel = seitenzahl2[0].count("href")
+	if int(seitenzahlselected) < seitenwieviel:
+		nextpagelink1 = seitenzahl3[1].split('"')
+		nextpagelink = "http://tv1.alarab.net" + nextpagelink1[7]
+		addDir("("+seitenzahlselected+"/"+str(seitenwieviel)+") Next Page",nextpagelink,1,"http://wadeni.com/images/icons/0alarab-net.jpg")
 
-def index_series(url):
-	try:
-		url_mod=url
-		url_mod=url_mod.split("_")
+def getSerie(url):
+	openerx = urllib2.build_opener()
+	sockx = openerx.open(url)
+	contentx = sockx.read() 
+	sockx.close()
+	wieviele = contentx.count('<div class="video-box">')
+	teilen = contentx.split('<div class="video-box">')
+	for i in range(1,wieviele+1):
+		linkjetzt = teilen[i].split('"')
+		imgjetzt = linkjetzt[3]
+		urljetzt = "http://tv1.alarab.net/"+linkjetzt[1]
+		namejetzt = linkjetzt[5]
+		addDir(namejetzt,urljetzt,3,imgjetzt)
+	seitenzahl1 = contentx.split('<div class="pages"><center>')
+	seitenzahl2 = seitenzahl1[1].split("</div></center></div>")
+	seitenzahl3 = seitenzahl2[0].split('tsc_3d_button blue"')
+	seitenzahl4 = seitenzahl3[1].split(">")
+	seitenzahl5 = seitenzahl4[1].split("<")
+	seitenzahlselected = seitenzahl5[0]
+	seitenwieviel = seitenzahl2[0].count("href")
+	if int(seitenzahlselected) < seitenwieviel:
+		nextpagelink1 = seitenzahl3[1].split('"')
+		nextpagelink = "http://tv1.alarab.net" + nextpagelink1[7]
+		addDir("("+seitenzahlselected+"/"+str(seitenwieviel)+") Next Page",nextpagelink,2,"http://wadeni.com/images/icons/0alarab-net.jpg")
 		
-		for items in url_mod:
-			mooded= str( url_mod[0]).split("-")
-			for elements in mooded:
-				second= mooded[0]
-				first= mooded[1]
-		
-		for i in range(1,20):
-		
-			result_url=second+"-"+str(i)+"_"+url_mod[1]+"_"+url_mod[2]
-			req = urllib2.Request(result_url)
-			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-			response = urllib2.urlopen(req)
-			link=response.read()
-			url_ch=(re.compile('<img src="(.+?)" alt="(.+?)"  />').findall(link))
-			url_ch_2=(re.compile('<a href="(.+?)" title="').findall(link))
-			final_series=[]
-			response.close()
-			for items in url_ch_2:
-				if "series" in items:
-					if items not in final_series:
-						final_series.append(items)
-			for items,elements in itertools.izip(url_ch,final_series):
-				image= items[0]
-				name= items [1]
-				url_serie=elements
-				#print name
-				#print "http://tv1.alarab.net"+url_serie
-				addDir(name,"http://tv1.alarab.net"+str(url_serie),2,image)
-	except Exception:
-		print "Exception in index_series "
+def getSerieFolge(url):
+	openerx = urllib2.build_opener()
+	sockx = openerx.open(url)
+	contentx = sockx.read() 
+	sockx.close()
+	wieviele = contentx.count('<div class="video-box">')
+	teilen = contentx.split('<div class="video-box">')
+	for i in range(1,wieviele+1):
+		linkjetzt = teilen[i].split('"')
+		imgjetzt = linkjetzt[3]
+		urljetzt = "http://tv1.alarab.net/"+linkjetzt[1]
+		namejetzt = linkjetzt[5]
+		addLink(namejetzt,urljetzt,4,imgjetzt)	
+	seitenzahl1 = contentx.split('<div class="pages"><center>')
+	seitenzahl2 = seitenzahl1[1].split("</div></center></div>")
+	seitenzahl3 = seitenzahl2[0].split('tsc_3d_button blue"')
+	seitenzahl4 = seitenzahl3[1].split(">")
+	seitenzahl5 = seitenzahl4[1].split("<")
+	seitenzahlselected = seitenzahl5[0]
+	seitenwieviel = seitenzahl2[0].count("href")
+	if int(seitenzahlselected) < seitenwieviel:
+		nextpagelink1 = seitenzahl3[1].split('"')
+		nextpagelink = "http://tv1.alarab.net" + nextpagelink1[7]
+		addDir("("+seitenzahlselected+"/"+str(seitenwieviel)+") Next Page",nextpagelink,3,"http://wadeni.com/images/icons/0alarab-net.jpg")	
 
-def list_eposodes(url):
-    try:
-		for counter in range(1,8):
-			req = urllib2.Request(url+"_"+str(counter))
-			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-			response = urllib2.urlopen(req)
-			link=response.read()
-			url_ch=(re.compile('<a href="(.+?)" title="(.+?)" class="vd_title">').findall(link))
-			response.close()
-			item_list=[]
-			for items in url_ch:
-				for element in items:
-					if items[1] not in item_list:
-						
-						name= str(items[1])
-						name=name.replace("تحميل", "")
-						name=name.replace("مسلسل", "")
-						name=name.replace("مدبلجة", "")
-						name=name.replace("مدبلجة", "")
-						name=name.replace("بالعربية", "")
-						name=name.replace("لاين", "")
-						name=name.replace("كاملة", "")
-						name=name.replace("مدبلجة", "")
-						name=name.replace("مترجم", "")
-						name=name.replace("فيلم", "")
-						name=name.replace("dvd", "")
-						name=name.replace("لابن", "")
-						name=name.replace("مشاهدة", "")
-						name=name.replace("اونلاين", "")
-						name=name.replace("بجودة", "")
-						name=name.replace("عالية", "")
-						name=name.replace("مباشرة", "")
-						name=name.replace("على", "")
-						name=name.replace("العرب", "")
-						name=name.replace("تحميل", "")
-						name=name.replace("جودة", "")
-						name=name.replace("كامل", "")
-						name=name.replace("بدون", "")
-						name=name.replace("اون", "")
-						name=name.replace("كواليتي", "")
-						name=name.strip()
-						
-						#print name
-						#print items[0]
-						addLink(name,"http://tv1.alarab.net"+str(items[0]),3,"")
-						item_list.append(items[1])
-    except Exception:
-		print "Exception in list_epos "
-					
-def list_films(url):
-	try:
-		url_mod=url
-		url_mod=url_mod.split("_")
-		
-		for items in url_mod:
-			mooded= str( url_mod[0]).split("-")
-			for elements in mooded:
-				second= mooded[0]
-				first= mooded[1]
-		
-		for i in range(1,20):
-		
-			result_url=second+"-"+str(i)+"_"+url_mod[1]+"_"+url_mod[2]
-			req = urllib2.Request(result_url)
-			req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-			response = urllib2.urlopen(req)
-			link=response.read()
-			response.close()
-			url_ch=(re.compile('<a href="(.+?)" title="(.+?)dvd اونلاين "').findall(link))
-			print url_ch
-			url_ch_2=(re.compile('<img src="(.+?)" alt="(.+?)"  />').findall(link))
-			 
-			for items,elements in itertools.izip( url_ch,url_ch_2):
-				
-				name=str( items[1]).replace("جودة", "")
-				name=name.replace("مشاهدة", "").strip()
-				name=name.replace("dvd", "")
-				name=name.replace("فيلم", "")
-				name=name.replace("لاين", "")
-				name=name.replace("اون", "")
-				name=name.replace("اونلاين", "")
-				name=name.replace("بجودة", "")
-				name=name.replace("عالية", "")
-				name=name.replace("مباشرة", "")
-				name=name.replace("على", "")
-				name=name.replace("العرب", "")
-				name=name.replace("تحميل", "")
-				name=name.replace("جودة", "")
-				name=name.replace("كواليتي", "")
-				name=name.replace("بدون", "")
-				name=name.replace("كامل", "")
-				name=name.strip()
-				addLink(name,"http://tv1.alarab.net"+str(items[0]),3,elements[0])
-	except Exception:
-		print "Exception in list_films "
-	
-				
+def PlayMovie(url):
+		opener = urllib2.build_opener()
+		sock = opener.open(url)
+		content = sock.read() 
+		sock.close()
+		source1 = content.split('http://alarabplayers.alarab.net')
+		source2 = source1[1].split('"')
+		filewrong = "http://alarabplayers.alarab.net"+source2[0]
+		opener2 = urllib2.build_opener()
+		sock2 = opener.open(filewrong)
+		content2 = sock2.read() 
+		sock2.close()
+		source3 = content2.split("'file': '")
+		source4 = source3[1].split("'")
+		fileright = source4[0]
+		print fileright
+		listItem = xbmcgui.ListItem(path=str(fileright))
+		xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
 
-def get_epos_video(url,name):
-	try:
-		url=str(url).split("_")
-    
-		tnumber=str( url[0])
-		tnumber=tnumber.replace("http://tv1.alarab.net/viewVedio/","")
-		tnumber=tnumber.replace("http://tv1.alarab.net/v", "")
-		tnumber=tnumber.replace("-", "").strip()
-		tnumber="http://alarabplayers.alarab.net/test.php?vid="+tnumber
-		
-		req = urllib2.Request(tnumber)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-		response = urllib2.urlopen(req)
-		link=response.read()
-		
-		response.close()
-		url_ch=(re.compile("'file': '(.+?)',").findall(link))
-		url_ch=str(url_ch).replace("['", "")
-		video=str(url_ch).replace("']", "").strip()
-		
-		image=(re.compile("'image': '(.+?)',").findall(link))
-		image=str(image).replace("['", "")
-		image=str(image).replace("']", "").strip()
-		
-		if "www.youtube" in video:
-			video=video.split("v=")
-			print "youtube after split: "+str(video)
-			video_id=str(video[1])
-			video_id=video_id.replace(".flv","").strip()
-			print "first item of youtube: "+str(video_id)
-			playback_url = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s' % video_id 
-			
-			listItem = xbmcgui.ListItem(path=str(playback_url))
-			xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
-			
-			
-		else:
-			video = str(video)+'|Referer=http://alarabplayers.alarab.net/jwplayer/player.swf'
-			listItem = xbmcgui.ListItem(path=video)
-			xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
-			
-	except Exception:
-		print "Exception in get_epos_video "
-			
-	
-
-	
+                
 def get_params():
         param=[]
         paramstring=sys.argv[2]
@@ -257,14 +132,6 @@ def get_params():
 
 
 
-
-def addLinkOLD(name,url,iconimage):
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
-        return ok
-
 def addLink(name,url,mode,iconimage):
     u=_pluginName+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
     ok=True
@@ -274,6 +141,8 @@ def addLink(name,url,mode,iconimage):
     ok=xbmcplugin.addDirectoryItem(handle=_thisPlugin,url=u,listitem=liz,isFolder=False)
     return ok
 	
+
+
 def addDir(name,url,mode,iconimage):
         u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
         ok=True
@@ -310,19 +179,18 @@ print "Name: "+str(name)
 if mode==None or url==None or len(url)<1:
         print ""
         CATEGORIES()
-       
 elif mode==1:
-        print ""+url
-        index_series(url)
-	
+	print ""+url
+	getMovie(url)
 elif mode==2:
-        print ""+url
-        list_eposodes(url)
+	print ""+url
+	getSerie(url)
 elif mode==3:
-		get_epos_video(url,name)
+	print ""+url
+	getSerieFolge(url)
 elif mode==4:
-		list_films(url)
+	print ""+url
+	PlayMovie(url)
 
-	
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
